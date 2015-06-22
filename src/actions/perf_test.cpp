4bfdf6dfac423fd043b46ec085bdf06296765741
Thorsten Tarrach <thorstent@live.com>
2015-06-22 21:28:29 +0200

    counts the number of locked instructions in Liss v1

--- src/actions/perf_test.cpp
+++ src/actions/perf_test.cpp
@@ -23,6 +23,7 @@
 #include "automata/perf_test_old.h"
 #include <iostream>
 #include <chrono>
+#include <unordered_set>
 
 using namespace std;
 using namespace std::chrono;
@@ -50,8 +51,43 @@ double perf_test::run_time_old(const cfg::program& program, bool concurrent, boo
   return time_span.count();
 }
 
+unsigned count_in_thread(const cfg::abstract_cfg& thread) {
+  
+  unordered_set<abstraction::psymbol> locked1;
+  
+  std::deque<pair<state_id_type,bool>> frontier;
+  frontier.push_back(make_pair(1,false));
+  while (!frontier.empty()) {
+    pair<state_id_type,bool> p = frontier.front();
+    state_id_type next = p.first;
+    bool locked = p.second;
+    frontier.pop_front();
+    const cfg::state& s = thread.get_state(next);
+    // check if s is locking
+    bool locking = s.action && s.action->variable_name=="l1" && s.action->operation==abstraction::op_class::lock;
+    bool unlocking = s.action && s.action->variable_name=="l1" && s.action->operation==abstraction::op_class::unlock;
+    assert (!locking || !unlocking);
+    assert (!unlocking || locked);
+    if (locked && !unlocking && s.action) locked1.insert(s.action.get());
+    for (const cfg::edge e : thread.get_successors(next)) {
+      if (!e.back_edge) { 
+        frontier.push_back(make_pair(e.to, (locked || locking) && !unlocking));
+        //const cfg::state& to = thread.get_state(e.to);
+      }
+    }
+  }
+  return locked1.size();
+}
+
 void perf_test::run(const cfg::program& program, clang::CompilerInstance& compiler)
 {
+  
+  unsigned total = 0;
+  // count locked by l1 instructions (this is only some hack to get some numbers for the paper)
+  for (const auto& t : program.minimised_threads())
+    total = total + count_in_thread(*t);
+  
+  cout << "Locked by l1: " << total << endl;
   /*cout << "Time for sequential with epsilon transitions: ";
   cout << run_time(program, false, false) << "s" << endl;
   
@@ -61,13 +97,13 @@ void perf_test::run(const cfg::program& program, clang::CompilerInstance& compil
   //}*/
   
   //if (verbosity==-1) {
-  cout << "Time for concurrent with epsilon transitions: ";
+  /*cout << "Time for concurrent with epsilon transitions: ";
   cout << run_time(program, true, false) << "s" << endl;
   //}
 
   //if (verbosity==-1) {
   cout << "Time for concurrent with epsilon transitions (old): ";
-  cout << run_time_old(program, true, false) << "s" << endl;
+  cout << run_time_old(program, true, false) << "s" << endl;*/
   //}
   
   /*//if (verbosity==-2) {
