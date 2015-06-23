5f1dd37bde9ec604079d84e89e496865c171c9a2
Thorsten Tarrach <thorstent@live.com>
2015-06-23 16:20:39 +0200

    produces proper exception

--- libs/z3++.h
+++ libs/z3++.h
@@ -28,6 +28,7 @@ Notes:
 #include<sstream>
 #include<z3.h>
 #include<limits.h>
+#include <exception>
 
 /**
    \defgroup cppapi C++ API
@@ -78,11 +79,14 @@ namespace z3 {
     /**
        \brief Exception used to sign API usage errors.
     */
-    class exception {
+    class exception : public std::exception {
         std::string m_msg;
     public:
         exception(char const * msg):m_msg(msg) {}
         char const * msg() const { return m_msg.c_str(); }
+        virtual const char* what() const noexcept override {
+          return m_msg.c_str();
+        }
         friend std::ostream & operator<<(std::ostream & out, exception const & e) { out << e.msg(); return out; }
     };
 
@@ -141,8 +145,10 @@ namespace z3 {
         */
         void check_error() const {
             Z3_error_code e = Z3_get_error_code(m_ctx);
-            if (e != Z3_OK)
-                throw exception(Z3_get_error_msg_ex(m_ctx, e));
+            if (e != Z3_OK) {
+              Z3_string str = Z3_get_error_msg_ex(m_ctx, e);
+                throw exception(str);
+            }
         }
 
         /**
